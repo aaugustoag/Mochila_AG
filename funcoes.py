cd54d11525597d5fdb09d473b5d26cc7c63bd486
Alexandre XC <49174397+aaugustoag@users.noreply.github.com>
2019-04-28 23:57:55 -0300

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,6 @@
 from random import randint
 from copy import deepcopy
 
-
 # imprimindo geração
 def imprime_geracao (populacao, g):
     print("Geração" + str(g) + "(" + str(len(populacao)) + "):" + str([item[0] for item in populacao]))
@@ -30,37 +29,29 @@ def verifica_candidatos (populacao, mochila):
 
 # gerando individuo guloso
 def guloso (itens, mochila):
-    for item in itens:
-        item.append(0)
-
-    individuo = [[0, 0]]
-
+    individuo = [[0, 0, 0]]
     while individuo[0][1] < mochila:
         maior = [0, 1]
         for i, item in enumerate(itens):
-            if (item[2] == 0):
+            if not (i in individuo):
                 if (item[0] / item[1] > maior[0] / maior[1]):
                     maior = item
                     maior_indice = i
-        itens[maior_indice][2] = 1
         individuo.append(maior_indice)
         individuo[0][0] += maior[0]
         individuo[0][1] += maior[1]
     individuo[0][0] -= itens[individuo[-1]][0]
     individuo[0][1] -= itens[individuo[-1]][1]
     individuo.pop()
-    individuo[1:] = sorted(individuo[1:])
-
-    for item in itens:
-        item.pop()
     return individuo
 # fim
 
 # gerando individuo aleatorio
 def populacao_aleatoria (itens, mochila):
-    individuo = [[0, 0]]
+    individuo = [[0, 0, 0]]
+    item = 0
     while individuo[0][1] < mochila:
-        item = randint(0, len(itens)-1)
+        item = randint(0, int(len(itens))-1)
         if not (item in individuo):
             individuo.append(item)
             individuo[0][0] += itens[item][0]
@@ -83,7 +74,6 @@ def elitismo (populacao, mochila):
                 maior = ind[0]
                 maior_indice = i
     populacao_e.append(deepcopy(populacao[maior_indice]))
-    populacao.remove(ind)
     return populacao_e
 # fim
 
@@ -93,32 +83,36 @@ def cruzamento (populacao, itens, tx_cruzamento):
     while len(populacao) > 1:
         rand = randint(0,len(populacao)-1)
         populacao_c.append(populacao[rand])
-        populacao_c.append(populacao[(rand+len(populacao))%len(populacao)])
+        populacao_c.append(populacao[(rand+1)%len(populacao)])
+        populacao.remove(populacao_c[-1])
+        populacao.remove(populacao_c[-2])
         if randint(0, 99) <= tx_cruzamento * 100:
             filhos = []
-            filhos.append(populacao[rand])
-            filhos.append(populacao[(rand+len(populacao))%len(populacao)])
-            for i in range(int(len(itens) / 2)):
-                if (i in filhos[0][1:]):
-                    if not (i in filhos[1][1:]):
-                        filhos[1].append(i)
-                        filhos[1][0][0] += itens[i][0]
-                        filhos[1][0][1] += itens[i][1]
-                        filhos[0].remove(i)
-                        filhos[0][0][0] -= itens[i][0]
-                        filhos[0][0][1] -= itens[i][1]
-                else:
-                    if (i in filhos[1][1:]):
-                        filhos[0].append(i)
-                        filhos[0][0][0] += itens[i][0]
-                        filhos[0][0][1] += itens[i][1]
-                        filhos[1].remove(i)
-                        filhos[1][0][0] -= itens[i][0]
-                        filhos[1][0][1] -= itens[i][1]
+            filhos.append(populacao_c[-1])
+            filhos.append(populacao_c[-2])
+            qde_cromossomos = 10
+            #print("pai" + str(filhos))
+            for i in range(qde_cromossomos):
+                for cromossomo in filhos[0][1:]:
+                    if not (cromossomo in filhos[1][1:]):
+                        filhos[1].append(cromossomo)
+                        filhos[1][0][0] += itens[cromossomo][0]
+                        filhos[1][0][1] += itens[cromossomo][1]
+                        filhos[0].remove(cromossomo)
+                        filhos[0][0][0] -= itens[cromossomo][0]
+                        filhos[0][0][1] -= itens[cromossomo][1]
+                        break
+                for cromossomo in filhos[1][1:]:
+                    if not (cromossomo in filhos[0][1:]):
+                        filhos[0].append(cromossomo)
+                        filhos[0][0][0] += itens[cromossomo][0]
+                        filhos[0][0][1] += itens[cromossomo][1]
+                        filhos[1].remove(cromossomo)
+                        filhos[1][0][0] -= itens[cromossomo][0]
+                        filhos[1][0][1] -= itens[cromossomo][1]
+                        break
             populacao_c.append(deepcopy(filhos[0]))
             populacao_c.append(deepcopy(filhos[1]))
-        populacao.remove(populacao[rand])
-        populacao.remove(populacao[(rand+len(populacao))%len(populacao)])
     if len(populacao) > 0:
         populacao_c.append(populacao[0])
 
@@ -128,10 +122,10 @@ def cruzamento (populacao, itens, tx_cruzamento):
 # fazendo mutacao
 def mutacao (populacao, itens, tx_mutacao):
     populacao_m = []
-    qde_cromossomos = int(len(itens)/5)
     for ind in populacao:
         if randint(0, 99) <= tx_mutacao * 100:
             mutante = deepcopy(ind)
+            qde_cromossomos = 10
             cromossomo = randint(0, len(itens)-1)
             for i in range(qde_cromossomos):
                 if int((cromossomo + i * qde_cromossomos) % len(itens)) in mutante:
@@ -149,54 +143,63 @@ def mutacao (populacao, itens, tx_mutacao):
     return populacao_m
 # fim
 
-# fazendo competicao roleta x5
+# fazendo competicao roleta x1
 def selecao (populacao, mochila, tam_pop):
     populacao_s = []
-    valor_total = 0
-    peso_total = 0
+    populacao_aux = []
     valor_acumulado = 0
-    populacao_s.append(populacao[0])
     maior_v = 0
     maior_p = 0
 
-    populacao = sorted(populacao)
-
     for ind in populacao:
-        if ind[0][0] == 0:
-            populacao.remove(ind)
-        valor_total += ind[0][0]
-        peso_total += ind[0][1]
+        ind[0][2] = 0
         if ind[0][0] > maior_v:
             maior_v = ind[0][0]
+        menor_v = maior_v
+        if ind[0][0] < menor_v:
+            menor_v = ind[0][0]
         if ind[0][1] > maior_p:
             maior_p = ind[0][1]
 
+    populacao = (sorted(populacao, reverse=True))
+
     #funcao aptidao
-    for ind in populacao:
-        valor_acumulado += int(ind[0][0] - valor_total / ind[0][1] * (ind[0][1] - mochila) + valor_total)
-        ind[0].append(valor_acumulado)
-    print(populacao)
+    for i,ind in enumerate(populacao):
+        if i > 0:
+            if sorted(populacao[i][1:]) != sorted(populacao[i-1][1:]):
+                if ind[0][1] > mochila:
+                    ind[0][2] = int(menor_v * ind[0][0] / maior_v / (ind[0][1] - mochila +1) + menor_v/10)
+                    #print(ind, menor_v, int(menor_v * ind[0][0] / maior_v / (ind[0][1] - mochila) + menor_v/10), valor_acumulado)
+                else:
+                    ind[0][2] = int(ind[0][0] + menor_v/10)
+                    #print(ind, menor_v, int(ind[0][0] + menor_v/10), valor_acumulado)
+                valor_acumulado += ind[0][2]
+                ind[0][2] = valor_acumulado
+                populacao_aux.append(deepcopy(ind))
+        else:
+            if ind[0][1] > mochila:
+                ind[0][2] = int(menor_v * ind[0][0] / maior_v / (ind[0][1] - mochila +1) + menor_v/10)
+                #print(ind, menor_v, int(menor_v * ind[0][0] / maior_v / (ind[0][1] - mochila) + menor_v/10), valor_acumulado)
+            else:
+                ind[0][2] = int(ind[0][0] + menor_v/10)
+                #print(ind, menor_v, int(ind[0][0] + menor_v/10), valor_acumulado)
+            valor_acumulado += ind[0][2]
+            ind[0][2] = valor_acumulado
+            populacao_aux.append(deepcopy(ind))
 
-    #selecao por roleta de 5
-    roleta = 5
+    #print("aux" + str(populacao_aux))
+
+    #selecao por roleta de 1
+    vitorioso = 0
     while len(populacao_s) < tam_pop-1:
-        vitorioso = [int(randint(0, int(valor_acumulado)))]
-        for i in range(roleta-1):
-            vitorioso.append(vitorioso[i]+int(valor_acumulado)%roleta)
+        vitorioso = (vitorioso + randint(0, int(valor_acumulado / 2))) % valor_acumulado
         for ind in populacao:
-            for i in range(roleta):
-                if (vitorioso[i] != 0):
-                    if ind[0][2] >= vitorioso[i]:
-                        populacao_s.append(ind)
-                        vitorioso[i] = 0
-    while len(populacao_s) > tam_pop:
+            if ind[0][2] >= vitorioso:
+                populacao_s.append(ind)
+                break
+
+    while len(populacao_s) > tam_pop-1:
         populacao_s.pop()
-    for ind in populacao:
-        ind[0].pop()
-
-    print(populacao_s)
-
-    populacao_s = sorted(populacao_s, reverse=True)
 
     return populacao_s
 # fim
--- principal.py
+++ principal.py
@@ -5,109 +5,116 @@ from copy import deepcopy
 #        0     1     2     3     4     5     6     7     8     9     10    11
 #itens2=[[3,5],[7,2],[4,5],[6,8],[3,6],[6,6],[7,1],[1,7],[1,1],[3,3],[3,2],[4,1]]
 
-itens=[]
 
 #instancia = input('Selecione a instancia:\n1- 40 itens:\n2- 100 itens:\n3- 10.000 itens:\n4- 10.000-2 itens:\n5- 11.000 itens:\n6- 100.000 iten:\n')
 
 #instancia = int(instancia)
-instancia = 1
 
-if instancia == 1:
-    print("Arquivo KNAPDATA40.TXT selecionado")
-    #/home/alexandreaag/PycharmProjects/Mochila_AG/Principal/
-    arq = open('KNAPDATA40.TXT', 'r')
-elif instancia == 2:
-    print("Arquivo KNAPDATA100.TXT selecionado")
-    arq = open('KNAPDATA100.TXT', 'r')
-elif instancia == 3:
-    print("Arquivo KNAPDATA10000.TXT selecionado")
-    arq = open('KNAPDATA10000.TXT', 'r')
-elif instancia == 4:
-    print("Arquivo KNAPDATA10000_2.TXT selecionado")
-    arq = open('KNAPDATA10000_2.TXT', 'r')
-elif instancia == 5:
-    print("Arquivo KNAPDATA11000.TXT selecionado")
-    arq = open('KNAPDATA11000.TXT', 'r')
-elif instancia == 6:
-    print("Arquivo KNAPDATA100000.TXT selecionado")
-    arq = open('KNAPDATA100000.TXT', 'r')
-else:
-    exit()
+for i in range(1):
+    itens=[]
+    instancia = i+6
 
-texto = arq.readlines()
-mochila = int(texto[0])
-for linha in texto[2:]:
-    linha_split=linha.split(",")
-    linha_split[2]=linha_split[2][:-1]
-    itens.append([int(linha_split[2]),int(linha_split[1])])
-arq.close()
+    if instancia == 1:
+        print("Arquivo KNAPDATA40.TXT selecionado")
+        #/home/alexandreaag/PycharmProjects/Mochila_AG/Principal/
+        arq = open('KNAPDATA40.TXT', 'r')
+    elif instancia == 2:
+        print("Arquivo KNAPDATA100.TXT selecionado")
+        arq = open('KNAPDATA100.TXT', 'r')
+    elif instancia == 3:
+        print("Arquivo KNAPDATA10000.TXT selecionado")
+        arq = open('KNAPDATA10000.TXT', 'r')
+    elif instancia == 4:
+        print("Arquivo KNAPDATA10000_2.TXT selecionado")
+        arq = open('KNAPDATA10000_2.TXT', 'r')
+    elif instancia == 5:
+        print("Arquivo KNAPDATA11000.TXT selecionado")
+        arq = open('KNAPDATA11000.TXT', 'r')
+    elif instancia == 6:
+        print("Arquivo KNAPDATA100000.TXT selecionado")
+        arq = open('KNAPDATA100000.TXT', 'r')
+    else:
+        exit()
 
-tam_pop=100
-qde_geracoes=100
-tx_mutacao=0.1
-tx_cruzamento=0.7
+    texto = arq.readlines()
+    mochila = int(texto[0])
+    for linha in texto[2:]:
+        linha_split=linha.split(",")
+        linha_split[2]=linha_split[2][:-1]
+        itens.append([int(linha_split[2]),int(linha_split[1])])
+    arq.close()
 
-populacao=[]
-individuo=[[0,0]]
+    tam_pop=10
+    qde_geracoes=50
+    tx_mutacao=0.1
+    tx_cruzamento=0.7
+    dispersao=[]
+    n_aplicacoes = 11
 
-dispersao=[]
-convergencia=[]
-geracoes=[]
+    for t in range(n_aplicacoes):
+        populacao=[]
+        individuo=[[0,0]]
+        convergencia=[]
+        valor_minimo=[]
 
-#gerando melhor individuo com guloso
-populacao.append(funcoes.guloso(itens,mochila))
-#fim
+        #gerando melhor individuo com guloso
+        #populacao.append(funcoes.guloso(itens,mochila))
+        #fim
 
-#gerando o restante da populacao inicial
-for i in range(tam_pop):
-    populacao.append(funcoes.populacao_aleatoria(itens,mochila))
-#fim
+        #gerando o restante da populacao inicial
+        for i in range(tam_pop):
+            populacao.append(funcoes.populacao_aleatoria(itens,mochila))
+        #fim
 
-g=0
-funcoes.imprime_geracao(populacao,g)
+        g=0
+        #funcoes.imprime_geracao(populacao,g)
 
-# armazenando possiveis candidatos
-dispersao.append(funcoes.verifica_candidatos(populacao, mochila))
-if funcoes.verifica_candidatos(populacao, mochila) != 0:
-    convergencia.append(max(funcoes.verifica_candidatos(populacao, mochila)))
-# fim
-
-for g in range(qde_geracoes):
-
-    populacao_e=funcoes.elitismo(populacao, mochila)
-    populacao_c=funcoes.cruzamento(populacao,itens,tx_cruzamento)
-    populacao_m=funcoes.mutacao(populacao,itens,tx_mutacao)
-
-    populacao=deepcopy(populacao_e)+deepcopy(populacao_c)+deepcopy(populacao_m)
-    #funcoes.imprime_cruzamento(populacao_g, g+1)
-    #funcoes.imprime_mutacao(populacao_m, g+1)
-
-    populacao_s=funcoes.selecao(populacao,mochila,tam_pop)
-    populacao=deepcopy(populacao_s)
-    funcoes.imprime_geracao(populacao, g+1)
-
-    # armazenando possiveis candidatos
-    if g%(100) == 0:
-        dispersao.append(funcoes.verifica_candidatos(populacao, mochila))
-    if funcoes.verifica_candidatos(populacao, mochila) != 0:
+        # armazenando possiveis candidatos
         convergencia.append(max(funcoes.verifica_candidatos(populacao, mochila)))
-    # fim
+        valor_minimo.append(min(funcoes.verifica_candidatos(populacao, mochila)))
+        # fim
 
-print("Melhor Solução: " + str(funcoes.elitismo(populacao, mochila)[0]))
+        for g in range(qde_geracoes):
 
-'''
-# plota boxplots das geracoes
-fig = plt.figure(1, figsize=(12, 6))
-ax = fig.add_subplot(111)
-plt.xlabel("Geracoes x 10")
-plt.ylabel("Dispersao")
-bp = ax.boxplot(dispersao)
-# fim
-# plota curva de convergencia
-fig = plt.figure(2, figsize=(12, 6))
-plt.plot(convergencia)
-plt.xlabel("Geracoes")
-plt.ylabel("Melhor Resultado")
+            populacao_e=funcoes.elitismo(populacao, mochila)
+            populacao_c=funcoes.cruzamento(populacao,itens,tx_cruzamento)
+            populacao=deepcopy(populacao_e)+deepcopy(populacao_c)
+            #funcoes.imprime_cruzamento(populacao_c, g+1)
+
+            populacao_m=funcoes.mutacao(populacao,itens,tx_mutacao)
+            populacao=deepcopy(populacao_e)+deepcopy(populacao_m)+deepcopy(populacao_c)
+            #funcoes.imprime_mutacao(populacao_m, g+1)
+
+            populacao_s=funcoes.selecao(populacao,mochila,tam_pop)
+            populacao=deepcopy(populacao_e)+deepcopy(populacao_s)
+            #funcoes.imprime_geracao(populacao, g+1)
+            #print("Melhor individuo" + str(g+1) + ": " + str(funcoes.elitismo(populacao, mochila)[0][0]))
+
+            # armazenando possiveis candidatos
+            convergencia.append(max(funcoes.verifica_candidatos(populacao, mochila)))
+            valor_minimo.append(min(funcoes.verifica_candidatos(populacao, mochila)))
+            # fim
+
+        melhor_solucao = funcoes.elitismo(populacao, mochila)
+        melhor_solucao[0][0].pop()
+        print("Melhor Solução" + str(t+1) + ": " + str(melhor_solucao)[2:-1])
+        dispersao.append(max(convergencia))
+
+        # plota curvas de convergencia
+        if t==0:
+            plt.figure('Convergencias' + str(instancia), figsize=(12, 10 * n_aplicacoes))
+        plt.subplot(n_aplicacoes, 1, t+1)
+        plt.plot(convergencia, 'b')
+        plt.plot(valor_minimo, 'r')
+        plt.xlabel("Geracoes")
+        plt.ylabel(str(funcoes.elitismo(populacao, mochila)[0][0][0]))
+        # fim
+
+    # plota boxplots das geracoes
+    fig = plt.figure('Dispercao' + str(instancia), figsize=(10, 6))
+    ax = fig.add_subplot(111)
+    plt.xlabel('Dispersao: ' + str(min(dispersao)) + '~' + str(max(dispersao)) + '\n' + str(dispersao))
+    plt.ylabel("Melhores Solucoes x 11")
+    bp = ax.boxplot(dispersao)
 plt.show()
-# fim
-'''
+    # fim
